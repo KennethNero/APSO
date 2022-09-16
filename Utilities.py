2eb2df155edf41d16b9eca64054912dda4ee236f
Kenneth Nero <kenero2@gmail.com>
2022-09-16 12:23:28 -0400

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -6,34 +6,9 @@ def sigmoid(x):
     return 1 / (1 + exp(-x))
 
 
-def get_probs(x):
-    """
-    Gets the confidence array of a given file without obfuscation. Baseline fitness
-
-    Parameters
-    ----------
-    A filename to pass in - our.apk
-
-    Returns
-    -------
-    An array in the form [prediction, confidence]
-
-    """
-    # Terminating condition for a successful attack, prediction = 1
-    # As the attack progresses, focus on label 2
-    # print("Single_dan_test with "+str(x)+"::")
-    cmd = "sudo bash /root/Automation/single_dan_test.sh " + str(x) + " /root/Automation/init_test/ 0"
-    # print("\t\'"+str(cmd)+"\'")
-    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    conf = out.split()[-1]  # Make sure this works
-    label = out.split()[-2]
-
-    # print("\t\tDEBUG:: get_probs - out = '"+str(out)+"'")
-    return [int(label), float(conf)]
-
 
 def fitnessScore(x, baselineConfidence):
+    # Takes in APK and baseline confidence.
     label, conf = get_probs(x)
     fitness = baselineConfidence - conf
     return fitness, conf, label
